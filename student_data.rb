85f6516ed332152fa1e971294849b62b7f796182
Keelin Hunt <huntkd@s.dcsdk12.org>
2017-09-12 10:44:19 -0600

    Functions 1-3

--- student_data.rb
+++ student_data.rb
@@ -11,7 +11,53 @@ File.open("student_data.csv").each do |line|
 	ages.push(info[2].strip.to_i)
 end
 
-print students, "\n"
-print eye_colors, "\n"
-print ages, "\n"
+# print students, "\n"
+# print eye_colors, "\n"
+# print ages, "\n"
 
+
+def count_brown_eyes (eye_colors)
+	n = 0
+	eye_colors.each do |eye_color|
+		if eye_color == "Brown"
+			n += 1
+		end
+	end
+
+	return n
+end
+
+# puts count_brown_eyes(eye_colors)
+
+def old_enough_to_drive (ages, students)
+	drivers = []
+	ages.each_with_index do |age, i|
+		if age >= 16
+			drivers.push(students[i])
+		end
+	end
+	return drivers
+end
+
+# print old_enough_to_drive(ages, students)
+
+
+def green_eyed_girls (students, eye_colors)
+	girl_list = []
+	green_eyed_girl_list = []
+	students.each_with_index do |students, i|
+		if i%2 == 0
+			girl_list.push(eye_colors[i])
+			girl_list.push(students[i])
+		end
+	end
+	girl_list.each_with_index do |eye_color, i|
+		if eye_color == "Green"
+			green_eyed_girl_list.push(students[i])
+		end
+	end
+
+	return green_eyed_girl_list
+end
+
+print green_eyed_girls(students, eye_colors)
